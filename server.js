a38ca00485ca997e2a9a3ce519678768574da17d
Spencer Gamero <bluerayman23@gmail.com>
2026-04-29 18:00:39 -0700

    updated server.js

--- server.js
+++ server.js
@@ -3,23 +3,18 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+// Setup Express server FIRST
+const app = express();
+
 // declare important variables
 const name = process.env.NAME;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.send(`Welcome, ${name}!`);
-});
-
 const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 
-// Setup Express server
-
-const app = express();
-
-//Serve static files from the public directory
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set EJS as the templating engine
@@ -30,13 +25,15 @@ app.set('views', path.join(__dirname, 'src/views'));
 
 // Declare Routes
 app.get('/', (req, res) => {
-    const title = 'Welcome Home';
+    const title = `Welcome, ${name}!`;
     res.render('home', { title });
 });
+
 app.get('/about', (req, res) => {
     const title = 'About Me';
     res.render('about', { title });
 });
+
 app.get('/products', (req, res) => {
     const title = 'Our Products';
     res.render('products', { title });
@@ -44,5 +41,5 @@ app.get('/products', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.1:${PORT}`);
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
